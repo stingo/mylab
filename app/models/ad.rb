af11097a997d617c34fd7adde58997220ace1f1b
Shania Dicen <shaniadicendev@gmail.com>
2020-02-25 17:52:37 +0800

    Add annotation to models

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: ads
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :text
+#  image       :string
+#  user_id     :integer
+#  slug        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  price       :decimal(, )
+#
 class Ad < ApplicationRecord
   belongs_to :user
 
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id           :bigint           not null, primary key
+#  title        :string
+#  published_on :date
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
