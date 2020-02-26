119fedc61fa2280a399c67ef809416e55d4d52cc
Shania Dicen <shaniadicendev@gmail.com>
2020-02-27 02:03:32 +0800

    Update money config initializer

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,6 +1,12 @@
-require 'money'
-require 'money/bank/google_currency'
-require 'json'
+require "money"
+require "money/bank/google_currency"
 
-MultiJson.engine = :json_gem
-Money.default_bank = Money::Bank::GoogleCurrency.new
+MoneyRails.configure do |config|
+  config.default_currency = :usd
+
+  Money::Bank::GoogleCurrency::SERVICE_HOST = "finance.google.com".freeze
+
+  # set default bank to instance of GoogleCurrency
+  Money::Bank::GoogleCurrency.ttl_in_seconds = 86_400
+  config.default_bank = Money::Bank::GoogleCurrency.new
+end
