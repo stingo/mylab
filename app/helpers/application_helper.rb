049e39138215671173a76dec4e3d618f5d85005e
Shania Dicen <shaniadicendev@gmail.com>
2020-02-27 03:15:45 +0800

    Update converted_price
    Add updating of rates in each conversion
    Remove converted_prive in Application helper and add it to ads helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def converted_price(price)
-    if session[:currency].present?
-      humanized_money_with_symbol(price.exchange_to(session[:currency]))
-    else
-      humanized_money_with_symbol(price)
-    end
-  end
 end
