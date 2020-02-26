77c78593fe69c90d5e84718380d932e7fda76a27
Shania Dicen <shaniadicendev@gmail.com>
2020-02-27 01:41:23 +0800

    Update annotations

--- test/models/ad_test.rb
+++ test/models/ad_test.rb
@@ -2,15 +2,16 @@
 #
 # Table name: ads
 #
-#  id          :bigint           not null, primary key
-#  title       :string
-#  description :text
-#  image       :string
-#  user_id     :integer
-#  slug        :string
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  price       :decimal(, )
+#  id             :bigint           not null, primary key
+#  title          :string
+#  description    :text
+#  image          :string
+#  user_id        :integer
+#  slug           :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  price_cents    :integer          default("0"), not null
+#  price_currency :string           default("USD"), not null
 #
 require 'test_helper'
 
