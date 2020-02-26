0c30c9ce813c87b4b04474a9d052fc80c089065b
Shania Dicen <shaniadicendev@gmail.com>
2020-02-27 02:03:39 +0800

    Add route to save currency

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :users
     resources :articles
     root "ads#index"
+    post "/controller/save_currency", to: "controller#save_currency"
   end
 end
