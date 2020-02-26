b2476cdea36703f6910f96b82023990774c9b9e8
Shania Dicen <shaniadicendev@gmail.com>
2020-02-27 03:16:05 +0800

    Add price in model

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -19,6 +19,19 @@ class Ad < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  monetize :price_cents
+
+  def price
+    Money.new price_cents, price_currency
+  end
+
+  def price=(value)
+    value = Money.parse(value) if value.instance_of? String # otherwise assume, that value is a Money object
+
+    self[:price_cents] = value.cents
+    self[:price_currency] = value.currency_as_string
+  end
+
   def should_generate_new_friendly_id?
     title_changed?
   end
