ba7e38394563843ba3e5d54ccb4d92bcee85bfc7
Shania Dicen <shaniadicendev@gmail.com>
2020-02-27 02:04:02 +0800

    Add price convert in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def converted_price(price)
+    if session[:currency].present?
+      humanized_money_with_symbol(price.exchange_to(session[:currency]))
+    else
+      humanized_money_with_symbol(price)
+    end
+  end
 end
