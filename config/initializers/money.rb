57de899b9c2400592c323fee55b408c8e28826c2
Shania Dicen <shaniadicendev@gmail.com>
2020-02-27 20:44:18 +0800

    Update initializers

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -3,6 +3,7 @@ require "eu_central_bank"
 
 MoneyRails.configure do |config|
   config.default_currency = :usd
+  Money.infinite_precision = true
 
   # set default bank to instance of EuCentralBank
   Money.default_bank = EuCentralBank.new
