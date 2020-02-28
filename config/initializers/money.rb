f8a635a576992448b4af92c973641b3bb20c9417
Shania Dicen <shaniadicendev@gmail.com>
2020-02-28 13:33:21 +0800

    Add configuration for currency layer
    Remove configuration of eu central bank

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,10 +1,13 @@
 require "money"
-require "eu_central_bank"
+require "money/bank/currencylayer_bank"
 
 MoneyRails.configure do |config|
   config.default_currency = :usd
-  Money.infinite_precision = true
 
-  # set default bank to instance of EuCentralBank
-  Money.default_bank = EuCentralBank.new
+  mclb = Money::Bank::CurrencylayerBank.new
+  mclb.access_key = "9e2fbbe64e3c0868487eaa5883c97d22"
+  mclb.ttl_in_seconds = 86_400
+  mclb.update_rates
+
+  config.default_bank = mclb
 end
