b559913ce256425155861c722504bbf5202f4d1c
Shania Dicen <shaniadicendev@gmail.com>
2020-03-11 07:47:10 +0800

    Add geocoder gem
    Fix code layout with rubocop

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -1,7 +1,7 @@
 module AdsHelper
   def converted_price(price)
     Money.default_bank.update_rates
-    
+
     if session[:currency].present?
       humanized_money_with_symbol(Money.default_bank.exchange_with(price, session[:currency]))
     else
