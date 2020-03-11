693eba0b0196c37fc8d6ca290dd7ff06cc4dff37
Shania Dicen <shaniadicendev@gmail.com>
2020-03-12 06:35:14 +0800

    Add service object filter currency
    Fix variable initalizations

---
+++ app/services/filter_currency.rb
@@ -0,0 +1,12 @@
+class FilterCurrency
+  def initializer(currency_code)
+    default_currency = "USD"
+    supported_currencies = %w[USD PHP EUR] # Add the currencies you want supported here (currencies in the dropdown menu of the application)
+
+    if supported_currencies.include? currency_code
+      currency_code
+    else
+      default_currency
+    end
+  end
+end
