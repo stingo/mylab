fd50d8889ea33c5345396283c7849411b917293a
Shania Dicen <shaniadicendev@gmail.com>
2020-03-12 07:28:44 +0800

    Debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
         @currency_code = @country_details.currency_code # To get currency code
         @filtered_currency = FilterCurrency.new(@currency_code) # This calls the service object and determines whether the currency code is supported
 
-        session[:currency] = @filtered_currency
+        session[:currency] = @currency_code
       end
     else
       @country = current_user.location
