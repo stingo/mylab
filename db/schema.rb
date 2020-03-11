2eee48b1faebef7ce3ccd5287d821aebfee3973c
Shania Dicen <shaniadicendev@gmail.com>
2020-03-12 02:43:58 +0800

    Add location to user migration
    Update schema with added user location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_173309) do
+ActiveRecord::Schema.define(version: 2020_03_11_174414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_173309) do
     t.string "first_name"
     t.string "last_name"
     t.string "country"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug"
