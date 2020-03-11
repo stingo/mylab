1aa33b63c392a9858b5a90ee840905fcb29f1990
Shania Dicen <shaniadicendev@gmail.com>
2020-03-12 06:48:15 +0800

    Add JPY to supported currencies

--- app/services/filter_currency.rb
+++ app/services/filter_currency.rb
@@ -1,7 +1,7 @@
 class FilterCurrency
   def initializer(currency_code)
     default_currency = "USD"
-    supported_currencies = %w[USD PHP EUR] # Add the currencies you want supported here (currencies in the dropdown menu of the application)
+    supported_currencies = %w[USD PHP EUR JPY] # Add the currencies you want supported here (currencies in the dropdown menu of the application)
 
     if supported_currencies.include? currency_code
       currency_code
