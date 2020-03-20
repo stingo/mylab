0f257a20afff54ba60e19e5fbbddd51c69dade5b
Shania Dicen <shaniadicendev@gmail.com>
2020-03-21 05:42:13 +0800

    Remove currency from ads

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_202159) do
+ActiveRecord::Schema.define(version: 2020_03_20_213358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(version: 2020_03_17_202159) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_cents", default: 0, null: false
     t.string "price_currency", default: "USD", null: false
-    t.bigint "currency_id", default: 1, null: false
-    t.index ["currency_id"], name: "index_ads_on_currency_id"
   end
 
   create_table "articles", force: :cascade do |t|
@@ -69,5 +67,4 @@ ActiveRecord::Schema.define(version: 2020_03_17_202159) do
     t.index ["username"], name: "index_users_on_username"
   end
 
-  add_foreign_key "ads", "currencies"
 end
