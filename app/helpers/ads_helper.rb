8b7a423aa5713898e87da2c36df261ebaab03f1e
Shania Dicen <shaniadicendev@gmail.com>
2020-03-21 05:19:23 +0800

    Add update rates if expired

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -1,6 +1,6 @@
 module AdsHelper
   def converted_price(price)
-    Money.default_bank.update_rates
+    MoneyRails.default_bank.update_rates if MoneyRails.default_bank.expired?
 
     if session[:currency].present?
       humanized_money_with_symbol(Money.default_bank.exchange_with(price, session[:currency]))
