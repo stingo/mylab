a300a1af6201a240eaee656ecf4e6befaf985c70
Shania Dicen <shaniadicendev@gmail.com>
2020-03-21 05:19:10 +0800

    Add currencylayer caching

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,7 +1,18 @@
-require "money"
-require "eu_central_bank"
+require "money/bank/currencylayer_bank"
 
 MoneyRails.configure do |config|
-  config.default_currency = :usd
-  Money.default_bank = EuCentralBank.new
+  bank = Money::Bank::CurrencylayerBank.new
+  bank.access_key = "749ea9046a7bd8b11d3e155d0acfdb19"
+  bank.source = "USD"
+  bank.ttl_in_seconds = 86_400
+  bank.cache = proc do |v|
+    key = "money:currencylayer_bank"
+    if v
+      Thread.current[key] = v
+    else
+      Thread.current[key]
+    end
+  end
+
+  config.default_bank = bank
 end
