45b6d874b0712e977e6280753998533bb80ed0b5
Shania Dicen <shaniadicendev@gmail.com>
2020-03-21 06:49:12 +0800

    Add currency link

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }, controllers: { registrations: "registrations" }
     resources :users
     resources :articles
-    resources :currencies, only: [:index]
+    resources :currencies, only: [:index, :show]
     root "ads#index"
     post "ads/save_currency", to: "ads#save_currency"
   end
